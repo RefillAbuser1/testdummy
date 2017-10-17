ea4f4ab2ddc6a8adeeeaabc8e6d1358b0284893c
RefillAbuser1 <32843024+RefillAbuser1@users.noreply.github.com>
2017-10-17 12:56:07 -0500

    Update testbot.js

--- testbot.js
+++ testbot.js
@@ -1,14 +1,11 @@
 const Discord = require("discord.js");
 const testbot = new Discord.Client();
+const ytdl = require("ytdl-core");
+const request = require("request");
+const fs = require("fs");
+const getYouTubeID = require("get-youtube-id");
+const fetchVideoInfo = require("yourtube-info");
 
-testbot.on('ready', () => {
-  console.log(`Logged in as ${testbot.user.tag}!`);
-});
-
-testbot.on('message', message => {
-  if (message.content === 'ping') {
-    message.reply('Pong!');
-  }
-});
+var config = JSON.parse(fs.readFileSync('./settings.json', 'utf-8'));
 
 testbot.login(process.env.BOT_TOKEN);
