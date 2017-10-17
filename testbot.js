7b94540aaee3a714b9e13a350792f51af3d8f287
RefillAbuser1 <32843024+RefillAbuser1@users.noreply.github.com>
2017-10-17 13:04:58 -0500

    Update testbot.js

--- testbot.js
+++ testbot.js
@@ -4,7 +4,7 @@ const ytdl = require("ytdl-core");
 const request = require("request");
 const fs = require("fs");
 const getYouTubeID = require("get-youtube-id");
-const fetchVideoInfo = require("yourtube-info");
+const fetchVideoInfo = require("youtube-info");
 
 var config = JSON.parse(fs.readFileSync('./settings.json', 'utf-8'));
 
