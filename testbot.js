f92f21f4713ec97743775483d0395885f3002d0b
RefillAbuser1 <32843024+RefillAbuser1@users.noreply.github.com>
2017-10-17 12:36:17 -0500

    Create testbot.js

---
+++ testbot.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+const testbot = new Discord.Client();
+
+testbot.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+testbot.on('message', message => {
+  if (message.content === 'ping') {
+    message.reply('Pong!');
+  }
+});
+
+testbot.login(process.env.BOT_TOKEN);
