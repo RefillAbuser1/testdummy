cf6487036589715f6bef4eb41ebce9f0b013c344
RefillAbuser1 <32843024+RefillAbuser1@users.noreply.github.com>
2017-10-17 12:59:55 -0500

    Update testbot.js

--- testbot.js
+++ testbot.js
@@ -8,4 +8,13 @@ const fetchVideoInfo = require("yourtube-info");
 
 var config = JSON.parse(fs.readFileSync('./settings.json', 'utf-8'));
 
+const yt_api_key = config.yt_api_key;
+const bot_controller = config.bot_controller;
+const prefix = config.prefix;
+const discord_token = config.discord_token;
+
 testbot.login(process.env.BOT_TOKEN);
+
+testbot.on('ready', function () {
+  console.log("testbot is online")
+}
