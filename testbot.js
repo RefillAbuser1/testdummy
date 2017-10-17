944c82c0c251ecff922d2c7a60a166933bd34148
RefillAbuser1 <32843024+RefillAbuser1@users.noreply.github.com>
2017-10-17 12:44:31 -0500

    Update testbot.js

--- testbot.js
+++ testbot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const testbot = new Discord.Client();
 
 testbot.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${testbot.user.tag}!`);
 });
 
 testbot.on('message', message => {
